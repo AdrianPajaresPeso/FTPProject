bd00100099b3cf313e38d67f7c9dca9bc42fdc41
Adrian Pajares Peso <dam08.2020.jesuitas@gmail.com>
2020-11-16 09:42:53 +0100

    falta poco

--- src/main/java/org/jesuitasrioja/ftpproject/ftpLayer/FTPOperationsImpl.java
+++ src/main/java/org/jesuitasrioja/ftpproject/ftpLayer/FTPOperationsImpl.java
@@ -33,6 +33,8 @@ public class FTPOperationsImpl implements FTPOperations {
 		}
 	}
 
+	
+	
 	@Override
 	public String directorioDeTrabajo() {
 		String cadenaRetorno = "";
@@ -56,18 +58,8 @@ public class FTPOperationsImpl implements FTPOperations {
 		boolean flag = false;
 		try {
 			FTPClient fclient = FTPUtils.getFTPConection();
-			System.out.println(fclient.printWorkingDirectory());
 			fclient.changeWorkingDirectory(path);
-			System.out.println(fclient.printWorkingDirectory());
-			System.out.println(fclient.getReplyCode());
-			// if(fclient.getReplyCode() == 200) {
-			
 			flag = true;
-			// }
-			if (fclient.getReplyCode() == 550) {
-				logger.error("No se ha podido cambiar de directorio" + fclient.getReplyCode());
-			}
-
 			fclient.disconnect();
 		} catch (SocketException e) {
 			// TODO Auto-generated catch block
@@ -89,9 +81,7 @@ public class FTPOperationsImpl implements FTPOperations {
 
 			cl.setFileType(FTP.BINARY_FILE_TYPE);
 			File fileToUpload = new File(path);
-			System.out.println(fileToUpload.getName());
-
-			System.out.println(cl.printWorkingDirectory());
+			
 			cl.storeFile(fileToUpload.getName(), new FileInputStream(fileToUpload));
 			System.out.println(cl.getReplyCode());
 
@@ -109,19 +99,49 @@ public class FTPOperationsImpl implements FTPOperations {
 
 	@Override
 	public Boolean bajarFichero(String file, String path) {
-		// TODO Auto-generated method stub
+		Boolean flag = false;
+		
+		try {
+			FTPClient cl = FTPUtils.getFTPConection();
+			
+			
+			cl.retrieveFile(file, new FileOutputStream(new File(path)));
+			
+			cl.disconnect();
+		} catch (SocketException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
 		return null;
 	}
 
 	@Override
 	public Boolean eliminarFichero(String file) {
-		// TODO Auto-generated method stub
+		try {
+			FTPClient cl = FTPUtils.getFTPConection();
+			cl.deleteFile(file);
+			cl.disconnect();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return null;
 	}
 
 	@Override
 	public Boolean crearCarpeta(String nombreCarpeta) {
-		// TODO Auto-generated method stub
+		try {
+			FTPClient cl = FTPUtils.getFTPConection();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		return null;
 	}
 
@@ -133,14 +153,42 @@ public class FTPOperationsImpl implements FTPOperations {
 
 	@Override
 	public List<FTPFile> listaFicherosCarpeta(String path) {
-		// TODO Auto-generated method stub
-		return null;
+		List<FTPFile> listaFicheros = null;
+		FTPClient cl;
+		try {
+			cl = FTPUtils.getFTPConection();
+
+			FTPFile[] files = cl.listFiles(path);
+			
+			for (FTPFile ftpFile : files) {
+				listaFicheros.add(ftpFile);
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return listaFicheros;
 	}
 
 	@Override
 	public List<FTPFile> listaDirectoriosCarpeta(String path) {
-		// TODO Auto-generated method stub
-		return null;
+		List<FTPFile> listaDirectorios = null;
+		FTPClient cl;
+		try {
+			cl = FTPUtils.getFTPConection();
+
+			FTPFile[] files = cl.listDirectories(path);
+			
+			for (FTPFile ftpFile : files) {
+				listaDirectorios.add(ftpFile);
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return listaDirectorios;
 	}
 
 	@Override
